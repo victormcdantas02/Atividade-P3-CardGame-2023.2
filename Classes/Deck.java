a6016285c84d9ac8c79d9cb4f985515f87417a86
victormcdantas02 <victor.00000844591@unicap.br>
2023-09-06 17:39:16 -0300

    Projeto Atualizado Deck, Lobby e Loja

--- Classes/Deck.java
+++ Classes/Deck.java
@@ -1,3 +1,17 @@
 public class Deck {
+        
+        private String nome;
+        private String[] cartas;
+        private boolean disponibilidade;
+        private String[] inventario;
+        private int quantidadeCartas;
+        
+        public Deck(String nome) {
+            this.nome = nome;
+            this.cartas = new String[60]; // Tamanho máximo de um deck
+            this.disponibilidade = false;
+            this.inventario = new String[60]; // Tamanho máximo do inventário
+            this.quantidadeCartas = 0;
+        }
     
 }
--- Classes/Lobby.java
+++ Classes/Lobby.java
@@ -1,3 +1,13 @@
 public class Lobby {
-    
+    private Deck[] decksDisponiveis; // Array de decks disponíveis
+    private Usuario[] jogadoresEmEspera; // Array de jogadores em espera
+    private int numDecks; // Número atual de decks disponíveis
+    private int numJogadoresEmEspera; // Número atual de jogadores em espera
+
+    public Lobby(int capacidadeMaxima) {
+        decksDisponiveis = new Deck[capacidadeMaxima];
+        jogadoresEmEspera = new Usuario[capacidadeMaxima];
+        numDecks = 0;
+        numJogadoresEmEspera = 0;
+    }
 }
