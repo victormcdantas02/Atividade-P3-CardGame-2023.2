f57a308b1df5bb0acb5bea4561e3bb5304133fab
victormcdantas02 <victor.00000844591@unicap.br>
2023-09-06 17:44:24 -0300

    Projeto todas as classes com base

--- Classes/Arena.java
+++ Classes/Arena.java
@@ -1,3 +1,10 @@
 public class Arena {
-    
+        private Usuario usuario1;
+        private Usuario usuario2;
+        private Deck deckJogador1;
+        private Deck deckJogador2;
+        private Carta[][] campoJogador1;
+        private Carta[][] campoJogador2;
+        private int vidaJogador1;
+        private int vidaJogador2;
 }
