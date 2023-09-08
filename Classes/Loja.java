43fe817081a07c368005056589df5e8c272e5ad1
victormcdantas02 <victor.00000844591@unicap.br>
2023-09-08 16:11:46 -0300

    Projeto Loja Atualizado

--- Classes/Loja.java
+++ Classes/Loja.java
@@ -1,10 +1,28 @@
 public class Loja {
     
     private String numeroCartao;
+   
+
     private String codigoVerificador;
 
     public Loja(String numeroCartao, String codigoVerificador) {
         this.numeroCartao = numeroCartao;
+        this.codigoVerificador = codigoVerificador;
+    }
+
+    public String getNumeroCartao() {
+        return numeroCartao;
+    }
+
+    public void setNumeroCartao(String numeroCartao) {
+        this.numeroCartao = numeroCartao;
+    }
+
+    public String getCodigoVerificador() {
+        return codigoVerificador;
+    }
+
+    public void setCodigoVerificador(String codigoVerificador) {
         this.codigoVerificador = codigoVerificador;
     }
     
