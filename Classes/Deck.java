7979dd8ebc72cd8ca5c739c79332eecb92411f21
victormcdantas02 <victor.00000844591@unicap.br>
2023-09-08 16:26:51 -0300

    Projeto Atualizado Deck e Loja

--- Classes/Deck.java
+++ Classes/Deck.java
@@ -1,16 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Deck {
     private String nome;
-    private int quantidadeDeCartas;
+    private int quantCartas;
     private boolean disponibilidade;
-    private Carta[] inventario;
+    private List<Carta> cartas;
 
     public Deck(String nome) {
         this.nome = nome;
+        this.quantCartas = quantCartas;
         this.disponibilidade = false;
-        this.inventario = new Carta[60];
+        this.cartas = new ArrayList<>();
     }
 
     public void compraDeBooster(Usuario usuario) {
-           }
-}
 
+    }
+
+    //Metodo GET
+    public String getNome(){
+        return nome;
+    }
+    public int quantidadeDeCartas(){
+        return quantCartas;
+    }
+    public boolean disponibilidade(){
+        return disponibilidade;
+    }
+    public List<Carta> getCartas(){
+        return cartas;
+    }
+
+    //Metodo SET
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public void setQuantidadeDeCartas(int quantCartas){
+        this.quantCartas = quantCartas;
+    }
+    public void setDisponibilidade(boolean disponibilidade){
+        this.disponibilidade = disponibilidade;
+    }
+    public void setCartas(List<Carta> cartas){
+        this.cartas = cartas;
+    }
+}
--- Classes/Loja.java
+++ Classes/Loja.java
@@ -27,5 +27,12 @@ public class Loja {
     }
     
     public void compraDeBooster(Usuario usuario, int cardcoins) {    
+        if (usuario.getCardcoins() >= cardcoins) {
+            usuario.setCardcoins(cardcoins);
+            System.out.println("Compra de booster bem-sucedida para o usuário: " + usuario.getUser());
+       }
+       else{
+        System.out.println("Cartões insuficientes. Não é possível comprar um booster para o usuário: " + usuario.getUser());
+       }  
     }
-}   
+} 
