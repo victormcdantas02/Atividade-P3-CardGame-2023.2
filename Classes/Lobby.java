fd46b0bf2ee10bb458effa09726caf0ff76be9cb
victormcdantas02 <victor.00000844591@unicap.br>
2023-09-13 10:33:00 -0300

    Projeto Atualizado

--- Classes/Lobby.java
+++ Classes/Lobby.java
@@ -1,6 +1,25 @@
 public class Lobby {
+    
     private Usuario[] jogadores;
     private int numJogadores;
-
     
+   
+    public Usuario[] getJogadores() {
+        return jogadores;
     }
+
+    public void setJogadores(Usuario[] jogadores) {
+        this.jogadores = jogadores;
+  
+    public int getNumJogadores() {
+        return numJogadores;
+    }
+
+    public void setNumJogadores(int numJogadores) {
+        this.numJogadores = numJogadores;
+
+    public Lobby(int maxJogadores) {
+        jogadores = new Usuario[maxJogadores];
+        numJogadores = 0;
+    }   
+}
