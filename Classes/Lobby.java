87ff627e68a01364fd12b9b864319e1188485924
Falcomate <F4lcomate@gmail.com>
2023-10-09 13:22:18 +0000

    Lobby dupla atualizado

--- Classes/Lobby.java
+++ Classes/Lobby.java
@@ -1,31 +1,18 @@
+import java.util.Random;
+
 public class Lobby {
-    
     private Usuario[] jogadores;
     private int numJogadores;
-    
-    public Usuario[] getJogadores() {
-        return jogadores;
-    }
+    private int maxJogadores;
 
-    public void setJogadores(Usuario[] jogadores) {
-        this.jogadores = jogadores;
-    }
-    
-    public int getNumJogadores() {
-        return numJogadores;
-    }
-
-    public void setNumJogadores(int numJogadores) {
-        this.numJogadores = numJogadores;
-    }
-    
     public Lobby(int maxJogadores) {
+        this.maxJogadores = maxJogadores;
         jogadores = new Usuario[maxJogadores];
         numJogadores = 0;
-    } 
+    }
 
     public void joinGame(Usuario player) {
-        if (numJogadores < jogadores.length) {
+        if (numJogadores < maxJogadores) {
             jogadores[numJogadores] = player;
             numJogadores++;
             matchPlayers(player);
@@ -47,5 +34,51 @@ public class Lobby {
 
     private void iniciarPartida(Usuario player, Usuario adversario) {
         System.out.println("Iniciando partida entre " + player.getNome() + " e " + adversario.getNome());
+    }
+
+    public void iniciarModoDupla() {
+        if (numJogadores < 4) {
+            System.out.println("Não há jogadores suficientes para o modo de dupla.");
+            return;
+        }
+
+        // Embaralhe os jogadores aleatoriamente
+        shufflePlayers();
+
+        // Divida os jogadores em duas equipes aleatórias
+        int equipe1Size = numJogadores / 2;
+        int equipe2Size = numJogadores - equipe1Size;
+
+        Usuario[] equipe1 = new Usuario[equipe1Size];
+        Usuario[] equipe2 = new Usuario[equipe2Size];
+
+        for (int i = 0; i < equipe1Size; i++) {
+            equipe1[i] = jogadores[i];
+        }
+
+        for (int i = equipe1Size; i < numJogadores; i++) {
+            equipe2[i - equipe1Size] = jogadores[i];
+        }
+
+        System.out.println("Equipe 1:");
+        for (Usuario jogador : equipe1) {
+            System.out.println(jogador.getNome());
+        }
+
+        System.out.println("Equipe 2:");
+        for (Usuario jogador : equipe2) {
+            System.out.println(jogador.getNome());
+        }
+    }
+
+    private void shufflePlayers() {
+        Random random = new Random();
+
+        for (int i = numJogadores - 1; i > 0; i--) {
+            int j = random.nextInt(i + 1);
+            Usuario temp = jogadores[i];
+            jogadores[i] = jogadores[j];
+            jogadores[j] = temp;
+        }
     }
 }
