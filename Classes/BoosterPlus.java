6ae508cebdaff2da81f307ab2b715b5b448738bd
Mateus-FCL <F4lcomate@gmail.com>
2023-10-18 20:08:15 -0300

    Atualização no BoosterPlus

--- Classes/BoosterPlus.java
+++ Classes/BoosterPlus.java
@@ -6,9 +6,9 @@ public class BoosterPlus extends Booster {
         double valorAleatorio = random.nextDouble() * 100;
 
         if (valorAleatorio <= 1.0) { // 1% de probabilidade de ser única
-            return new CartaUnique("NomeUnica", "ImagemUnica", "TipoUnica", Raridade.EPICA, null, 1, 1, 1, 1);
+            return new CartaUnique(false, "NomeUnica", "ImagemUnica", "TipoUnica", Raridade.EPICA, null, 1, 1, 1, 1);
         } else {
             return null; // Não é uma carta única
-        }
-    }
+        }
+    }
 }
