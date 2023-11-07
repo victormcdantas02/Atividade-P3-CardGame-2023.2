50d12b0061f5e36b1878d04c02a13989c5202eba
Victor Dantas <victormcdantas@gmail.com>
2023-11-07 13:30:41 +0000

    Projeto Atualizado

--- Classes/Progresso.java
+++ Classes/Progresso.java
@@ -1,3 +1,10 @@
 public abstract class Progresso {
-    
+    protected String[] premiacoes;
+    protected int premiacaoAtual;
+public Progresso(){
+    this.premiacoes = new String[60];
+    this.premiacaoAtual = 0;
+}
+    public abstract void entregarPremiacao();
+    public abstract void progresso();
 }
