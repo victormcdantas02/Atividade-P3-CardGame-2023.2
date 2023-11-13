ef9d959d8ddf27d3bbd20515f01ff93cd82c2e1e
aluno <aluno0@unicap.br>
2023-11-13 11:45:30 -0300

    Projeto Atualizado Correção da Classe progresso

--- Classes/Progresso.java
+++ Classes/Progresso.java
@@ -1,11 +1,11 @@
 public abstract class Progresso {
     protected String[] premiacoes;
     protected int premiacaoAtual;
- public Progresso() {
+    public Progresso() {
         this.premiacoes = new String[60];
         this.premiacaoAtual = 0;
     }
-    public abstract void entregarPremiacao();
+    public abstract void entregarPremiacao(Usuario user);
 
-    public abstract void progresso();
+    public abstract void progresso(Usuario user);
 }
