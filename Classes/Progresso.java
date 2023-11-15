bddbce1852b7b3b57a221bede84df23cc186cd2d
victormcdantas02 <victor.00000844591@unicap.br>
2023-11-14 21:53:15 -0300

    Projeto Atualizado Base para Excessões

--- Classes/Progresso.java
+++ Classes/Progresso.java
@@ -5,7 +5,7 @@ public abstract class Progresso {
         this.premiacoes = new String[60];
         this.premiacaoAtual = 0;
     }
-    public abstract void entregarPremiacao();
+    public abstract void entregarPremiacao(Usuario user);
 
-    public abstract void progresso();
+    public abstract void progresso(Usuario user);
 }
